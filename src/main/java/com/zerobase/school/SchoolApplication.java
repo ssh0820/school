dd0c9d5d2d01dea74b9debbdaedcbf939e6f56cb
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2023-11-04 23:40:19 +0900

    feat: mysql, application 설정 추가

--- src/main/java/com/zerobase/school/SchoolApplication.java
+++ src/main/java/com/zerobase/school/SchoolApplication.java
@@ -1,9 +1,19 @@
 package com.zerobase.school;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
+@EnableFeignClients
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
+@RequiredArgsConstructor
 public class SchoolApplication {
 
     public static void main(String[] args) {
